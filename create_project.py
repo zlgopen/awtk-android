42fa4434eb78d0e990a0c97736e24a115af421f0
xianjimli <xianjimli@gmail.com>
2020-02-03 21:49:18 +0800

    fix scripts

--- create_project.py
+++ create_project.py
@@ -129,7 +129,7 @@ def copy_app_files(config, app_root_dst, app_root_src):
         copy_file(sfrom, sto)
 
     sfrom =  join_path(app_root_src, assets_dir + '/default/raw');
-    sto = join_path(app_root_dst, 'app/src/main/assets/assets/raw');
+    sto = join_path(app_root_dst, 'app/src/main/assets/assets/default/raw');
     copy_folder(sfrom, sto);
 
 def config_get_app_full_name(config):
--- scripts/env_mac.sh
+++ scripts/env_mac.sh
@@ -2,4 +2,4 @@ export ANDROID_HOME=/Users/jim/Library/Android/sdk/
 export ANDROID_NDK_HOME=/Users/jim/Library/Android/sdk/ndk-bundle/
 export PLATFORM_TOOLS_HOME=/Users/jim/Library/Android/sdk/platform-tools
 
-export PATH=$PATH:$NDK_HOME:$PLATFORM_TOOLS_HOME
+export PATH=$PATH:$ANDROID_NDK_HOME:$PLATFORM_TOOLS_HOME
