8b2cf16ae0292a6ff425670b065523ee86a46f0f
xianjimli <xianjimli@hotmail.com>
2020-02-25 06:49:13 +0800

    add env.bat

--- scripts/env.bat
+++ scripts/env.bat
@@ -1,5 +1,5 @@
-set JAVA_HOME="c:\Program Files\Android\Android Studio\jre"
-set ANDROID_HOME="d:\androidstudio\sdk"
-set ANDROID_NDK_HOME="d:\androidstudio\sdk\ndk-bundle"
+set JAVA_HOME="c:\\Program Files\\Android\\Android Studio\\jre"
+set ANDROID_HOME="d:\\androidstudio\\sdk"
+set ANDROID_NDK_HOME="d:\\androidstudio\\sdk\\ndk-bundle"
 set Path=%Path%:%ANDROID_NDK_HOME%
 
