dfd807eae781ae1e443c9838750d86b35d0d15ca
lixianjing <xianjimli@hotmail.com>
2020-09-07 06:38:56 +0800

    add env_win.sh

---
+++ scripts/env_win.sh
@@ -0,0 +1,5 @@
+export ANDROID_HOME=/c/Users/Admin/AppData/Local/Android/Sdk
+export ANDROID_NDK_HOME=/c/Users/Admin/AppData/Local/Android/Sdk/ndk/21.3.6528147
+export PLATFORM_TOOLS_HOME=/c/Users/Admin/AppData/Local/Android/Sdk/platform-tools
+
+export PATH=$PATH:$ANDROID_NDK_HOME:$PLATFORM_TOOLS_HOME
