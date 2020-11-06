fa2b13f78abfb182f80e314f9a947ba515913a7e
lixianjing <jim@lixianjings-MacBook-Pro.local>
2020-11-06 18:52:39 +0800

    update env

--- scripts/env_mac.sh
+++ scripts/env_mac.sh
@@ -1,5 +1,5 @@
 export ANDROID_HOME=/Users/jim/Library/Android/sdk/
-export ANDROID_NDK_HOME=/Users/jim/Library/Android/sdk/ndk-bundle/
+export ANDROID_NDK_HOME=/Users/jim/Library/Android/sdk/ndk/21.3.6528147
 export PLATFORM_TOOLS_HOME=/Users/jim/Library/Android/sdk/platform-tools
 
 export PATH=$PATH:$ANDROID_NDK_HOME:$PLATFORM_TOOLS_HOME
